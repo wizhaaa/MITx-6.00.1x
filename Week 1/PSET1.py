4d38301f0ba6050c7a27d2079405cd5228b09b8d
will z <46132945+woppaa@users.noreply.github.com>
2020-06-10 15:01:22 -0400

    Update PSET1.py

--- Week 1/PSET1.py
+++ Week 1/PSET1.py
@@ -1,4 +1,8 @@
 # Paste your code into this box 
+#Assume s is a string of lower case characters.
+#Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. 
+#For example, if s = 'azcbobobegghakl', your program should print:
+#Number of vowels: 5
 
 count = 0 
 
