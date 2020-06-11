77e7b047176cbc0952c72c787d6c72dfcf9f914e
will z <46132945+woppaa@users.noreply.github.com>
2020-06-11 14:48:24 -0400

    Update Pset3.py

--- Week 1/Pset3.py
+++ Week 1/Pset3.py
@@ -8,8 +8,9 @@
 
 # Longest substring in alphabetical order is: abc
 # Note: This problem may be challenging. We encourage you to work smart. If you've spent more than a few hours on this problem, 
-# we suggest that you move on to a different part of the course. If you have time, come back to this problem after you've had a break and cleared your head.
-
+# we suggest that you move on to a different part of the course. If you have time, come back to this problem after you've had a 
+# break and cleared your head.
+#-----------------------------------------------------------------------------------------------------------------------------
 start = 0
 length = 1
 maxLen = 1
